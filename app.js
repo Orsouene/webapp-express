08d546027a3e37b234bbd4b8be0e512892d8ee17
Orsouene <orsoueneelaouizeb@gmail.com>
2025-01-23 16:50:03 +0100

    Bonus :Inserire i dati di connessione al database come variabili d’ambiente (Fatto d'all inizio )+ Corretto  bonus 1

--- app.js
+++ app.js
@@ -2,12 +2,21 @@
 const express = require("express");
 // Connessione al database
 const connection = require("./connection");
+// importato CORS sul app.js
+var cors = require("cors");
 // creao una istanza
 const app = express();
 // PORT
 const port = process.env.PORT || 3000;
 // importo il router
 const movieRouter = require("./routers/router");
+
+//Global Middleware:
+app.use(cors());
+// body parser  il body di qualunque richiesta va parsato come application/json
+app.use(express.json());
+// prendo l'im dalal cartella public
+app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.send("welcome to my home page");
 });
